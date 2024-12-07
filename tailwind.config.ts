c060237e0727fa85fc754a97d047ba138a64785e
Fth87 <fatihfwz87@gmail.com>
2024-12-07 22:34:47 +0700

    Fix tailwind issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,10 @@
 import type { Config } from 'tailwindcss';
-// tailwind.config.js
-import { nextui } from '@nextui-org/react';
+import { nextui } from '@nextui-org/theme';
 
 export default {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './{app,pages,components,src/{app,pages,components}}/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-
-    '../src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    '../src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    '../src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    '../node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -22,7 +14,6 @@ export default {
       },
     },
   },
-
   darkMode: 'class',
   plugins: [nextui()],
 } satisfies Config;
