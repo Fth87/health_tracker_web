789fd75868560c42b15db7bd57d5ca0f3c4455f3
Fth87 <fatihfwz87@gmail.com>
2024-12-07 21:48:02 +0700

    first commit

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 
-import { IconSvgProps } from "@/types";
+interface IconSvgProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+  width?: number;
+  height?: number;
+}
+
+
 
 export const Logo: React.FC<IconSvgProps> = ({
   size = 36,
