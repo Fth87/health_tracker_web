c0085153aa8cfbc22488b1db1d4e64cce99d1024
Fth87 <fatihfwz87@gmail.com>
2024-12-11 09:52:24 +0700

    fix: fix deployment problem

--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -74,7 +74,7 @@ export default function UploadPage() {
     formData.append('file', file);
 
     try {
-      const response = await axios.post('http://localhost:8000/api/laptops/upload', formData, {
+      await axios.post('http://localhost:8000/api/laptops/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -206,12 +206,12 @@ export default function UploadPage() {
   );
 }
 
-function CloudUploadIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
-      <path d="M12 12v9" />
-      <path d="m16 16-4-4-4 4" />
-    </svg>
-  );
-}
+// function CloudUploadIcon(props: React.SVGProps<SVGSVGElement>) {
+//   return (
+//     <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+//       <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+//       <path d="M12 12v9" />
+//       <path d="m16 16-4-4-4 4" />
+//     </svg>
+//   );
+// }
